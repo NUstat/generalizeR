5358ba2e0b76b3668be324c9aca9f1beb1330e0d
Tim Ruel <timruel88@gmail.com>
2021-10-07 09:21:48 -0500

    Updates

--- R/outcome_data.R
+++ R/outcome_data.R
@@ -7,19 +7,20 @@ selection_covariates = c("total", "pct_black_or_african_american", "pct_female",
 
 output = stratify(inference_pop, guided = FALSE, n_strata = 4, variables = selection_covariates, idnum = "ncessch")
 
-set.seed(3592)
+#set.seed(3592)
 
-stratum_sample_sizes = c(18, 7, 8, 7)
+stratum_sample_sizes = c(20, 4, 5, 11)
 total_sample_size = stratum_sample_sizes %>% sum()
 
-beta = c(0, 4, 8, 12, 16)
-delta = c(1, 1, 2, 3, 4)
+beta = c(0, 4, 8, 12, 16)*4
+delta = c(0.5, 1, 2, 3, 4)*4
 
 target_data = output$recruit_data %>%
-  mutate(total = scale(total),
-         pct_female = scale(pct_female),
-         pct_black_or_african_american = scale(pct_black_or_african_american),
-         pct_free_and_reduced_lunch = scale(pct_free_and_reduced_lunch)
+  mutate(
+    total = scale(total),
+    pct_female = scale(pct_female),
+    pct_black_or_african_american = scale(pct_black_or_african_american),
+    pct_free_and_reduced_lunch = scale(pct_free_and_reduced_lunch)
   )
 
 trial_data = target_data %>%
@@ -52,7 +53,7 @@ target_data = target_data %>%
                    (beta[3] + delta[3]*treatment)*pct_female +
                    (beta[4] + delta[4]*treatment)*pct_black_or_african_american +
                    (beta[5] + delta[5]*treatment)*pct_free_and_reduced_lunch +
-                   rnorm(1, 0, 1)
+                   rnorm(1, 0, 10)*(1+treatment)
          ) %>%
   dplyr::select(-Stratum, -rank, -ncessch)
 
