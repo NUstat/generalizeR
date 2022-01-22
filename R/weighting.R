1861af55b2fb2e56cbabfa5fc541b7a49f49a4af
Tim Ruel <timruel88@gmail.com>
2022-01-21 23:14:22 -0600

    Move code that generates propensity scores to separate function

--- R/weighting.R
+++ R/weighting.R
@@ -57,61 +57,10 @@ weighting <- function(data, trial, treatment, outcome, selection_covariates,
   data <- data %>% drop_na(c(trial, selection_covariates))
 
   ### Generate Participation Probabilities ###
-  # Logistic Regression
-  if(selection_method == "lr") {
+  ps <- generate_ps(data, trial, selection_covariates, selection_method)
 
-    formula <- paste(trial,
-                   paste(selection_covariates, collapse = "+"),
-                   sep = "~") %>%
-      as.formula()
-
-    ps <- formula %>%
-      glm(data = data,
-          family = "quasibinomial") %>%
-      predict(type = "response")
-  }
-
-  # Random Forest
-  if(selection_method == "rf") {
-
-    formula <- paste(
-      paste("as.factor(", trial, ")"),
-      paste(selection_covariates, collapse = "+"),
-      sep = "~") %>%
-      as.formula()
-
-    ps <- randomForest::randomForest(formula,
-                                     data = data,
-                                     na.action = na.omit) %>%
-                                     #sampsize = 454,
-                                     #ntree = 1500) %>%
-      predict(type = "prob") %>%
-      as.data.frame() %>%
-      pull("1")
-  }
-
-  # Lasso
-  if(selection_method == "lasso") {
-
-    test.x <- model.matrix(~ -1 + .,
-                           data = data %>% select(selection_covariates))
-
-    test.y <- data %>% pull(trial)
-
-    ps <- glmnet::cv.glmnet(x = test.x,
-                            y = test.y,
-                            family = "binomial") %>%
-      predict(newx = test.x,
-              s = "lambda.1se",
-              type = "response") %>%
-      as.numeric()
-  }
-
-  ### Set any participation probabilities of 0 in the trial to the minimum non-zero value ###
-
-  if(0 %in% ps) {
-    ps[which(data[,trial] == 1 & ps == 0)] <- min(ps[which(data[,trial] == 1 & ps != 0)], na.rm = TRUE)
-  }
+  participation_probs <- list(population = ps[which(data[,trial] == 0)],
+                              trial = ps[which(data[,trial] == 1)])
 
   ### Generate Weights ###
   if(is_data_disjoint == TRUE) {
