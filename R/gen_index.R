82282baf885ec532666b385e6b41a268b156132e
Tim Ruel <timruel88@gmail.com>
2022-01-21 23:09:59 -0600

    Make cosmetic changes to gen_index() code

--- R/gen_index.R
+++ R/gen_index.R
@@ -16,26 +16,37 @@
 gen_index <- function(trial_ps, pop_ps) {
   ##Baklizi and Eidous (2006) estimator
   # bandwidth
-  h = function(x){
-    n = length(x)
-    optim_binwidth = (4*sqrt(var(x))^5/(3*n))^(1/5)
-    if(is.na(optim_binwidth) | is.nan(optim_binwidth)){
-      optim_binwidth = 0
+
   if(var(trial_ps) == 0 & var(pop_ps) == 0) {return(1)}
-    }
-    if(optim_binwidth < 0.001){ # this yielded a b index of 0.9999501 for (at least one specific case of) "perfectly" stratified data
-      optim_binwidth = 0.001
-    }
-    return(optim_binwidth)
-  }
 
-  # kernel estimators of the density and the distribution
-  kg = function(x, data){
-    hb = h(data) #bin width
-    k = r = length(x)
-    for(i in 1:k) r[i] = mean(dnorm((x[i] - data)/hb))/hb # we divide by bin width, which is a problem when bin width goes to zero
-    return(r)
-  }
+  else {
 
-  return(as.numeric(integrate(function(x) sqrt(kg(x, dat1B)*kg(x, dat2B)), -Inf, Inf)$value))
+    h = function(x){
+
+      n = length(x)
+      optim_binwidth = (4*sqrt(var(x))^5/(3*n))^(1/5)
+
+      if(is.na(optim_binwidth) | is.nan(optim_binwidth)){
+
+        optim_binwidth = 0
+      }
+      if(optim_binwidth < 0.001) { # this yielded a b index of 0.9999501 for (at least one specific case of) "perfectly" stratified data
+
+        optim_binwidth = 0.001
+      }
+
+      return(optim_binwidth)
+    }
+
+    # kernel estimators of the density and the distribution
+    kg = function(x, data){
+
+      hb = h(data) #bin width
+      k = r = length(x)
+      for(i in 1:k) r[i] = mean(dnorm((x[i] - data)/hb))/hb # we divide by bin width, which is a problem when bin width goes to zero
+      return(r)
+    }
+
+    return(as.numeric(integrate(function(x) sqrt(kg(x, trial_ps)*kg(x, pop_ps)), -Inf, Inf)$value))
+  }
 }
